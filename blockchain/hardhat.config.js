67e4e41aebce8e8151eeb63c87dd56f64efaa3f6
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-05 18:19:06 -0300

    setup the hardhat config file
    Add optimization directives and configuration to use the Goerli Testnet.

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,36 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+require("hardhat-gas-reporter")
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    goerli: {
+      chainId: 5,
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      blockExplorerUrl: "https://goerli.etherscan.io/",
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
+  gasReporter: {
+    currency: 'ETH',
+    enabled: true
+  },
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  }
+}
