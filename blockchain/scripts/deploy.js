21426e8256c76a04b56efbf02a9f9d8383533701
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-06 17:56:08 -0300

    create deploy script
    Deploy all the contracts:
    - npx hardhat run scripts/deploy.js --network localhost
    
    The deployment script creates/updates the addresses of the contracts and its respective ABI files on the front-end folder.
    The addresses are stored in the config.json file on the src folder of the front-end part.
    The ABIs are located in a folder called abis also in the src folder of the front-end part.

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -5,27 +5,125 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const fs = require("fs"); // to copy the files to be used by the web interface
+
+
+let clinicalDataAddress
+let authorizedAccountsAddress
+let patientsDataAddress
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // Setup accounts
+  [ account1, account2, account3 ] = await ethers.getSigners()
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // deploy ClinicalData contract
+  const ClinicalData = await ethers.getContractFactory('ClinicalData')
+  const clinicalData = await ClinicalData.deploy()
+  await clinicalData.deployed();
+  clinicalDataAddress = clinicalData.address
+  console.log(`ClinicalData contract deployed to ${clinicalData.address}`);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // deploy AuthorizedAccounts contract
+  const AuthorizedAccounts = await ethers.getContractFactory('AuthorizedAccounts')
+  const authorizedAccounts = await AuthorizedAccounts.deploy()
+  await authorizedAccounts.deployed();
+  authorizedAccountsAddress = authorizedAccounts.address
+  console.log(`AuthorizedAccounts contract deployed to ${authorizedAccounts.address}`);
 
-  await lock.deployed();
+  // deploy PatientsData contract
+  const PatientsData = await ethers.getContractFactory('PatientsData')
+  const patientsData = await PatientsData.deploy()
+  await patientsData.deployed();
+  patientsDataAddress = patientsData.address
+  console.log(`PatientsData contract deployed to ${patientsData.address}`);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+
+  // Setup another authorized account
+  const setupAccount = await authorizedAccounts.setAuthorizedPerson('John', 'Doe', 98765432100, 199999999, '0xAD36Bf57B393f5673B7D57CB95f2F03E62b02F5D')
+  await setupAccount.wait()
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+
+// Function to copy the ABI files
+function createABIFiles() {
+  // ClinicalData ABI
+  let jsonFile = fs.readFileSync('./artifacts/contracts/ClinicalData.sol/ClinicalData.json')
+  let jsonData = JSON.parse(jsonFile);
+  let stringfyData = JSON.stringify(jsonData.abi, null, " ")
+  let abiFilePath = "../electron/src/abis/ClinicalData.json"
+  writeABIs(abiFilePath, stringfyData)
+
+  // AuthorizedAccounts ABI
+  jsonFile = fs.readFileSync('./artifacts/contracts/AuthorizedAccounts.sol/AuthorizedAccounts.json')
+  jsonData = JSON.parse(jsonFile);
+  stringfyData = JSON.stringify(jsonData.abi, null, " ")
+  abiFilePath = "../electron/src/abis/AuthorizedAccounts.json"
+  writeABIs(abiFilePath, stringfyData)
+
+  // PatientsData ABI
+  jsonFile = fs.readFileSync('./artifacts/contracts/PatientsData.sol/PatientsData.json')
+  jsonData = JSON.parse(jsonFile);
+  stringfyData = JSON.stringify(jsonData.abi, null, " ")
+  abiFilePath = "../electron/src/abis/PatientsData.json"
+  writeABIs(abiFilePath, stringfyData)
+}
+
+
+// write function
+function writeABIs(_destination, _data) {
+  // save new file
+  var options = { flag : 'w' };
+  fs.writeFileSync(_destination, _data , options, function(err) {
+    if (err) throw err;
+    console.log('complete');
+  })
+}
+
+
+// Function to create/ update config.json file
+function createConfigJSON(_clinicalDataAddress, _authorizedAccountsAddress, _patientsDataAddress) {
+  const configFilePath = "../electron/src/config.json";
+
+  // Create data JSON with contents
+  var data = {}
+  data[5] = [] //localhost
+
+  data[5] = {
+    clinicalData: {
+      address: _clinicalDataAddress
+    },
+    authorizedAccounts: {
+      address: _authorizedAccountsAddress
+    },
+    patientsData: {
+      address: _patientsDataAddress
+    }
+  }
+
+  // save new file
+  stringfyData = JSON.stringify(data, null, " ")
+  var options = { flag : 'w' };
+  fs.writeFileSync(configFilePath, stringfyData , options, function(err) {
+    if (err) throw err;
+    console.log('complete');
+  })
+}
+
+
+const runMain = async () => {
+  try {
+    await main()
+    // copy files to client-side
+    createABIFiles()
+    // create config.json with deployed addresses
+    createConfigJSON(clinicalDataAddress, authorizedAccountsAddress, patientsDataAddress)
+    // terminate without errors
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+runMain()
