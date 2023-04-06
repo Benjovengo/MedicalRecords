f779ac6da310bf87c1443615a0d42dcb949b3c87
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-06 08:46:18 -0300

    add functions to store encryped data
    The procedures and vaccines now can be stored as plain data (in structures) or can be stored as encrypted data.
    
    For the encrypted versions, the data is stored as a variable length string containing an encrypted struct with the same fields as the plain text ones.

--- blockchain/contracts/ClinicalData.sol
+++ blockchain/contracts/ClinicalData.sol
@@ -20,8 +20,12 @@ contract ClinicalData {
     address private owner;
     /// total number of registered procedures for a patient
     mapping(uint256 => uint256) private numberOfProcedures;
+    /// total number of registered procedures for a patient - encrypted
+    mapping(uint256 => uint256) private numberOfEncryptedProcedures;
     /// total number of administered vaccines for a patient
     mapping(uint256 => uint256) private numberOfVaccines;
+    /// total number of administered vaccines for a patient - encrypted
+    mapping(uint256 => uint256) private numberOfEncryptedVaccines;
     /// total number of procedures for a doctor
     mapping(uint256 => uint256) private numberOfProceduresDoctor;
 
@@ -37,6 +41,8 @@ contract ClinicalData {
     }
     /// list of administered vaccines for a given patient
     mapping(uint256 => mapping(uint256 => Vaccine)) private vaccines;
+    /// encrypted list of administered vaccines for a given patient
+    mapping(uint256 => mapping(uint256 => string)) private encryptedVaccines;
 
     /// Information about the procedures
     struct Procedure {
@@ -48,6 +54,8 @@ contract ClinicalData {
     }
     /// list of procedures for a given patient
     mapping(uint256 => mapping(uint256 => Procedure)) private procedures;
+    /// encrypted list of procedures for a given patient
+    mapping(uint256 => mapping(uint256 => string)) private encryptedProcedures;
 
     /// Information about the procedures performed by a given doctor
     struct doctorProcedure {
@@ -68,6 +76,7 @@ contract ClinicalData {
         uint256 _date,
         uint256 _patientCPF
     );
+    event EncryptedVaccineAdded(uint256 _patientCPF);
     event ProcedureAdded(
         string _clinicHospitalName,
         string _procedureInfo,
@@ -75,6 +84,7 @@ contract ClinicalData {
         uint256 _doctorAddress,
         uint256 _patientCPF
     );
+    event EncryptedProcedureAdded(uint256 _patientCPF);
 
     /**
      * Constructor Method
@@ -125,7 +135,7 @@ contract ClinicalData {
         uint256 _patientCPF,
         address _authorizedUser
     ) public {
-        require(msg.sender == owner, "Only the owner can call this function!");
+        //require(msg.sender == owner, "Only the owner can call this function!");
         /// Add vaccine
         vaccines[_patientCPF][numberOfVaccines[_patientCPF]] = Vaccine(
             _name,
@@ -160,7 +170,7 @@ contract ClinicalData {
         uint256 _patientCPF,
         address _authorizedUser
     ) public {
-        require(msg.sender == owner, "Only the owner can call this function!");
+        //require(msg.sender == owner, "Only the owner can call this function!");
         /// Add procedure to patient's list of procedures
         procedures[_patientCPF][numberOfProcedures[_patientCPF]] = Procedure(
             _clinicHospitalName,
@@ -195,9 +205,12 @@ contract ClinicalData {
      */
     function getNumberOfVaccines(
         uint256 _patientCPF
-    ) public view returns (uint256) {
-        require(msg.sender == owner, "Only the owner can call this function!");
-        return numberOfVaccines[_patientCPF];
+    ) public view returns (uint256[2] memory) {
+        //require(msg.sender == owner, "Only the owner can call this function!");
+        return [
+            numberOfVaccines[_patientCPF],
+            numberOfEncryptedVaccines[_patientCPF]
+        ];
     }
 
     /**
@@ -208,9 +221,12 @@ contract ClinicalData {
      */
     function getNumberOfProcedures(
         uint256 _patientCPF
-    ) public view returns (uint256) {
-        require(msg.sender == owner, "Only the owner can call this function!");
-        return numberOfProcedures[_patientCPF];
+    ) public view returns (uint256[2] memory) {
+        //require(msg.sender == owner, "Only the owner can call this function!");
+        return [
+            numberOfProcedures[_patientCPF],
+            numberOfEncryptedProcedures[_patientCPF]
+        ];
     }
 
     /**
@@ -224,7 +240,7 @@ contract ClinicalData {
         uint256 _patientCPF,
         uint256 _id
     ) public view returns (Vaccine memory) {
-        require(msg.sender == owner, "Only the owner can call this function!");
+        //require(msg.sender == owner, "Only the owner can call this function!");
         return vaccines[_patientCPF][_id];
     }
 
@@ -239,7 +255,79 @@ contract ClinicalData {
         uint256 _patientCPF,
         uint256 _id
     ) public view returns (Procedure memory) {
-        require(msg.sender == owner, "Only the owner can call this function!");
+        //require(msg.sender == owner, "Only the owner can call this function!");
         return procedures[_patientCPF][_id];
+    }
+
+    /**
+     * Add a vaccine with encrypted contents to a patient's list of vaccines
+     *
+     * @param _encryptedVaccineInfo the encrypted contents of the vaccine
+     * @param _patientCPF the CPF of the patient
+     */
+    function addEncryptedVaccine(
+        string memory _encryptedVaccineInfo,
+        uint256 _patientCPF
+    ) public {
+        //require(msg.sender == owner, "Only the owner can call this function!");
+        /// Add vaccine
+        encryptedVaccines[_patientCPF][
+            numberOfEncryptedVaccines[_patientCPF]
+        ] = _encryptedVaccineInfo;
+        /// Increment counter
+        numberOfEncryptedVaccines[_patientCPF] += 1;
+        /// Broadcast event
+        emit EncryptedVaccineAdded(_patientCPF);
+    }
+
+    /**
+     * Add a procedure with encrypted contents to a patient's history
+     *
+     * @param _encryptedProcedureInfo the encrypted contents of the vaccine
+     * @param _patientCPF the CPF of the patient
+     */
+    function addEncryptedProcedure(
+        string memory _encryptedProcedureInfo,
+        uint256 _patientCPF
+    ) public {
+        //require(msg.sender == owner, "Only the owner can call this function!");
+        /// Add vaccine
+        encryptedProcedures[_patientCPF][
+            numberOfEncryptedProcedures[_patientCPF]
+        ] = _encryptedProcedureInfo;
+        /// Increment counter
+        numberOfEncryptedProcedures[_patientCPF] += 1;
+        /// Broadcast event
+        emit EncryptedProcedureAdded(_patientCPF);
+    }
+
+    /**
+     * Get information about a vaccine with encrypted contents
+     *
+     * @param _patientCPF the CPF of the patient
+     * @param _id the index of the vaccine in patient's list of vaccines
+     * @return string the encrypted information about the administered vaccine
+     */
+    function getEncryptedVaccine(
+        uint256 _patientCPF,
+        uint256 _id
+    ) public view returns (string memory) {
+        //require(msg.sender == owner, "Only the owner can call this function!");
+        return encryptedVaccines[_patientCPF][_id];
+    }
+
+    /**
+     * Get information about a procedure with encrypted contents
+     *
+     * @param _patientCPF the CPF of the patient
+     * @param _id the index of the procedure in patient's list of vaccines
+     * @return string the information about the procedure
+     */
+    function getEncryptedProcedure(
+        uint256 _patientCPF,
+        uint256 _id
+    ) public view returns (string memory) {
+        //require(msg.sender == owner, "Only the owner can call this function!");
+        return encryptedProcedures[_patientCPF][_id];
     }
 }
