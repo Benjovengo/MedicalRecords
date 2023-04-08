f6e73a3fda5f71711ee170e3554beea0cbfdaf7d
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-08 09:27:55 -0300

    add a script file to format a CPF number for displaying
    Formatting actions for a numeric string:
    - add a period between the third and sixth digits
    - add a dash before the last two digits

---
+++ electron/src/renderer/scripts/cpfFormatting.tsx
@@ -0,0 +1,20 @@
+/**
+   * Formats an 11-digit number as ***.***.***-**
+   * 
+   * @param CPF the string containing an 11-digit number - digits only
+   * @returns formatted CPF as ***.***.***-**
+   */
+const cpfFormatting = (CPF: String) => {
+  let CPFNumber = Number(CPF)
+  let formattedCPF = String(CPFNumber)
+  // Add a period after the third and sixth digits
+  formattedCPF = formattedCPF.replace(/(\d{3})(\d)/, "$1.$2");
+  formattedCPF = formattedCPF.replace(/(\d{3})(\d)/, "$1.$2");
+  // Add a dash before the last two digits
+  formattedCPF = formattedCPF.replace(/(\d{2})$/, "-$1");
+  
+  return formattedCPF
+}
+
+
+export default cpfFormatting
