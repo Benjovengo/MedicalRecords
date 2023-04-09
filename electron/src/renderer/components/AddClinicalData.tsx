ee54f04b498258e82181781ddb7533b022ac0f96
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-09 19:32:23 -0300

    add react hook to select the type of data to be entered
    The hook procedureOrVaccine is used to control which form will be displayed: the procedure or the vaccine form.
    
    A select-option HTML object controls the content of the hook using the function selectProcedureOrVaccine.

--- electron/src/renderer/components/AddClinicalData.tsx
+++ electron/src/renderer/components/AddClinicalData.tsx
@@ -10,11 +10,34 @@ interface AddClinicalDataProps {
 
 
 const AddClinicalData: React.FunctionComponent<AddClinicalDataProps> = ({ address, sharedCPF }) => {
+  /**
+   * Hooks
+   */
+  // Select which type of data form will be displayed
+  // Values: 'addProcedure' or 'addVaccine'
+  const [procedureOrVaccine, setProcedureOrVaccine] = useState('addProcedure')
   
-  
+
+  /**
+   * Set the hook for choosing between the Procedure or the Vaccine forms
+   * 
+   * @param event used to read the value of the select component
+   */
+  const selectProcedureOrVaccine = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setProcedureOrVaccine(event.target.value)
+  }
+
+
   return (
     <>
-      <h1>Procedures and Vaccines Data</h1>
+      <h3>Procedures and Vaccines Data</h3>
+      <div className='mt-3 ms-3'>
+        <select className='select__function' id="functionToCall" name="functionToCall" onChange={selectProcedureOrVaccine} >
+          <option value="addProcedure">Add Procedure</option>
+          <option value="addVaccine">Add Vaccine</option>
+        </select>
+      </div>
+
     </>
   )
 };
