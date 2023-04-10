7de0d7a4b800f487ea890cc30851e0ff6df2394f
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-10 13:47:28 -0300

    load the smart contracts
    Use the ethers library to interact with the smart contracts.
    
    -setup a provider and a signer to use the ClinicalData and the PatientsData smart contracts.

--- electron/src/renderer/components/RetrieveData.tsx
+++ electron/src/renderer/components/RetrieveData.tsx
@@ -2,7 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from "reactstrap";
 import { ethers } from 'ethers'
 
+
+/** Contract and Address */
+import ClinicalData from '../../abis/ClinicalData.json' // contract ABI
+import PatientsData from '../../abis/PatientsData.json' // contract ABI
+import config from '../../config.json' // contracts addresses
+import { HARDHAT_ACCOUNT01_PRIVATE_KEY } from '../../../work/sensitive';
+
+/** Scripts */
+import cpfFormatting from 'renderer/scripts/cpfFormatting';
+import { decryptText } from 'renderer/scripts/cryptography';
+
+/** Stylesheet */
 import "./RetrieveData.css"
+
+
+
+// Setup provider and network - Alchemy
+/* const alchemyProvider = new ethers.providers.AlchemyProvider("goerli", ALCHEMY_API_KEY);
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider); */
+
+// Setup provider and signer - Localhost
+const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
+const signer = new ethers.Wallet(HARDHAT_ACCOUNT01_PRIVATE_KEY, provider);
+
+const clinicalData = new ethers.Contract(config[31337].clinicalData.address, ClinicalData, signer)
+const patientsData = new ethers.Contract(config[31337].patientsData.address, PatientsData, signer)
 
 
 /**
